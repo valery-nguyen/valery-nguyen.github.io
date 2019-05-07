5cb41ace8299c72ed442e8ec560627e0e20f9986
valery-nguyen <valery.nguyen@ucla.edu>
2019-05-07 02:22:12 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 62
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $('.carousel').carousel({
+    interval: 3500
+  });
+
+  // Ruby-on-Rails Chart
+  var doughnutData = [{
+      value: 90,
+      color: "#1abc9c"
+    },
+    {
+      value: 10,
+      color: "#ecf0f1"
+    }
+  ];
+  var myDoughnut = new Chart(document.getElementById("ruby-on-rails").getContext("2d")).Doughnut(doughnutData);
+
+  // Javascript Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+
+  // React & Redux Chart
+  var doughnutData = [{
+    value: 90,
+    color: "#1abc9c"
+  },
+  {
+    value: 10,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("react-redux").getContext("2d")).Doughnut(doughnutData);
+
+  // MongoDB Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("mongodb").getContext("2d")).Doughnut(doughnutData);
+
+  // Express Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("express").getContext("2d")).Doughnut(doughnutData);
+
+  // Node Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("node").getContext("2d")).Doughnut(doughnutData);
+});
